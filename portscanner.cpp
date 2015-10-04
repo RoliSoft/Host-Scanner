3bd752658db0ae316663fc0cbe2f201b067180b5
RoliSoft <rolisoft@gmail.com>
2015-10-05 00:49:29 +0300

    Added reason output.

--- portscanner.cpp
+++ portscanner.cpp
@@ -7,7 +7,7 @@ void PortScanner::DumpResults(Services* services)
 {
 	for (auto service : *services)
 	{
-		cout << service->address << ":" << service->port << " is " << (service->alive ? "open" : "closed") << endl;
+		cout << service->address << ":" << service->port << " is " << (service->alive ? "open" : "closed") << " (" << service->reason << ")" << endl;
 
 		if (service->banlen > 0)
 		{

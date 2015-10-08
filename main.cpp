83041f45e1de38dd1ab8dc78e27b5387824863fc
RoliSoft <rolisoft@gmail.com>
2015-10-08 22:30:41 +0300

    Added ASCII art. Every serious project needs one.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
+#include <tuple>
 #include "stdafx.h"
 #include "utils.h"
-#include "udpscanner.h"
 
 using namespace std;
 
@@ -16,6 +16,22 @@ int main()
 		return EXIT_FAILURE;
 	}
 #endif
+
+	auto app = get<1>(splitPath(getAppPath()));
+	cout << "   _   _           _     _____                                 " << endl;
+	cout << "  | | | |         | |   /  ___|                                " << endl;
+	cout << "  | |_| | ___  ___| |_  \\ `--.  ___ __ _ _ __  _ __   ___ _ __ " << endl;
+	cout << "  |  _  |/ _ \\/ __| __|  `--. \\/ __/ _` | '_ \\| '_ \\ / _ \\ '__|" << endl;
+	cout << "  | | | | (_) \\__ \\ |_  /\\__/ / (_| (_| | | | | | | |  __/ |   " << endl;
+	cout << "  \\_| |_/\\___/|___/\\__| \\____/ \\___\\__,_|_| |_|_| |_|\\___|_|   " << endl;
+	cout << "                                                               " << endl;
+	cout << "           https://github.com/RoliSoft/Host-Scanner" << endl;
+	cout << endl;
+	cout << "usage: " << app << endl;
+	cout << "help: " << endl;
+	cout << endl;
+	cout << "  ! CLI is not yet implemented." << endl;
+	cout << endl;
 
 #if Windows
 	WSACleanup();

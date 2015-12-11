75c04d4021a6313d736745f6ea326586663f15d9
RoliSoft <rolisoft@gmail.com>
2015-12-12 01:40:07 +0200

    Nice-ified compiler script.

--- distrib/deb/compile.sh
+++ distrib/deb/compile.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-shopt -s extglob
+set -e
 set -x
+shopt -s extglob
 
-cd /root && \
-git clone https://github.com/RoliSoft/Host-Scanner && \
-cd Host-Scanner/build && \
-cmake .. && \
-make HostScanner && \
-rm -rf !(HostScanner) && \
-cd .. && \
-cpack -D CPACK_GENERATOR="${1^^}" && \
+cd /root
+git clone https://github.com/RoliSoft/Host-Scanner
+cd Host-Scanner/build
+cmake ..
+make HostScanner
+rm -rf !(HostScanner) .gitkeep
+cd ..
+cpack -D CPACK_GENERATOR="${1^^}"
 ../upload.sh *."${1,,}"

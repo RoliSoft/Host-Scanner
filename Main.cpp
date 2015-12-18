efd82b41bd276bd06d2680d6cd5bf78834bc404c
RoliSoft <rolisoft@gmail.com>
2015-12-19 00:03:59 +0200

    Added CLI argument parsing.

--- Main.cpp
+++ Main.cpp
@@ -21,7 +21,9 @@
 #include <iostream>
 #include <string>
 #include <tuple>
+#include <fstream>
 #include <boost/program_options.hpp>
+#include <boost/program_options/parsers.hpp>
 #include "Stdafx.h"
 #include "Utils.h"
 #include "Format.h"
@@ -36,7 +38,7 @@
 using namespace std;
 namespace po = boost::program_options;
 
-int main()
+int main(int argc, char *argv[])
 {
 #if Windows
 	WSADATA wsaData;
@@ -53,24 +55,67 @@ int main()
 
 	Format::Init();
 
-	po::options_description desc("arguments");
+	po::options_description desc("arguments", 120);
 	desc.add_options()
-		("help,h", "Shows this message.")
+		("target,t", po::value<vector<string>>(),
+			"List of targets to scan:\n"
+			"  Each can be a hostname, IP address, IP range or CIDR.\n"
+			"  E.g. `192.168.1.1/24` is equivalent to `192.168.1.0-192.168.1.255`.")
+		("port,p", po::value<string>(),
+			"Ports to scan:\n"
+			"  Can be a single port (80), a list (22,80) or a range (1-1024).\n"
+			"  Range can be unbounded from either sides, simultaneously.\n"
+			"  E.g. `1024-` will scan ports 1024-65535. `-` will scan all ports.")
+		("scanner,s", po::value<string>(),
+			"Scanner instance to use:\n"
+			"  internal - Uses the built-in scanner. (active)\n"
+			"  nmap     - Uses 3rd-party application Nmap. (active)\n"
+			"  shodan   - Uses data from Shodan. (passive; requires API key)\n"
+			"  censys   - Uses data from Censys. (passive; requires API key)")
+		("shodan-key", po::value<string>(), "Specifies an API key for Shodan.")
+		("censys-key", po::value<string>(), "Sepcifies an API key for Censys in the `uid:secret` format.")
+		("passive,p", "Globally disables active reconnaissance. Functionality using active scanning will break, but ensures no accidental active scans will be initiated, which might get construed as hostile.")
+		("no-logo,q", "Suppresses the ASCII logo.")
+		("version,v", "Display version information.")
+		("help,h", "Displays this message.")
 	;
 
-	auto app = get<1>(splitPath(getAppPath()));
-	cout << Format::Green;
-	cout << "  " << Format::Bold << " _   _ "  << Format::Normal << Format::Green << "          _    "   << Format::Bold << " _____"   << Format::Normal << Format::Green << "                                 "    << endl;
-	cout << "  " << Format::Bold << "| | | |"  << Format::Normal << Format::Green << "         | |   "   << Format::Bold << "/  ___|"  << Format::Normal << Format::Green << "                                "     << endl;
-	cout << "  " << Format::Bold << "| |_| |"  << Format::Normal << Format::Green << " ___  ___| |_  "   << Format::Bold << "\\ `--."  << Format::Normal << Format::Green << "  ___ __ _ _ __  _ __   ___ _ __ "    << endl;
-	cout << "  " << Format::Bold << "|  _  |"  << Format::Normal << Format::Green << "/ _ \\/ __| __|  " << Format::Bold << "`--. \\"  << Format::Normal << Format::Green << "/ __/ _` | '_ \\| '_ \\ / _ \\ '__|"  << endl;
-	cout << "  " << Format::Bold << "| | | |"  << Format::Normal << Format::Green << " (_) \\__ \\ |_  " << Format::Bold << "/\\__/ /" << Format::Normal << Format::Green << " (_| (_| | | | | | | |  __/ |   "     << endl;
-	cout << "  " << Format::Bold << "\\_| |_/" << Format::Normal << Format::Green << "\\___/|___/\\__| " << Format::Bold << "\\____/"  << Format::Normal << Format::Green << " \\___\\__,_|_| |_|_| |_|\\___|_|   " << endl;
-	cout << endl;
-	cout << "           " << Format::Bold << "https" << Format::Normal << Format::Green << "://" << Format::Bold << "github.com" << Format::Normal << Format::Green << "/" << Format::Bold << "RoliSoft" << Format::Normal << Format::Green << "/" << Format::Bold << "Host-Scanner" << Format::Normal << Format::Default << endl;
-	cout << endl;
-	cout << "usage: " << app << " [args]" << endl;
-	cout << desc << endl;
+	po::positional_options_description pos;
+	pos.add("target", -1);
+
+	po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).options(desc).positional(pos).run(), vm);
+
+	ifstream inifs("HostScanner.ini");
+	if (inifs.good())
+	{
+		po::store(po::parse_config_file(inifs, desc, true), vm);
+		inifs.close();
+	}
+
+	po::notify(vm);
+
+	if (vm.count("no-logo") == 0)
+	{
+		auto app = get<1>(splitPath(getAppPath()));
+		cout << Format::Green;
+		cout << "  " << Format::Bold << " _   _ "  << Format::Normal << Format::Green << "          _    "   << Format::Bold << " _____"   << Format::Normal << Format::Green << "                                 "    << endl;
+		cout << "  " << Format::Bold << "| | | |"  << Format::Normal << Format::Green << "         | |   "   << Format::Bold << "/  ___|"  << Format::Normal << Format::Green << "                                "     << endl;
+		cout << "  " << Format::Bold << "| |_| |"  << Format::Normal << Format::Green << " ___  ___| |_  "   << Format::Bold << "\\ `--."  << Format::Normal << Format::Green << "  ___ __ _ _ __  _ __   ___ _ __ "    << endl;
+		cout << "  " << Format::Bold << "|  _  |"  << Format::Normal << Format::Green << "/ _ \\/ __| __|  " << Format::Bold << "`--. \\"  << Format::Normal << Format::Green << "/ __/ _` | '_ \\| '_ \\ / _ \\ '__|"  << endl;
+		cout << "  " << Format::Bold << "| | | |"  << Format::Normal << Format::Green << " (_) \\__ \\ |_  " << Format::Bold << "/\\__/ /" << Format::Normal << Format::Green << " (_| (_| | | | | | | |  __/ |   "     << endl;
+		cout << "  " << Format::Bold << "\\_| |_/" << Format::Normal << Format::Green << "\\___/|___/\\__| " << Format::Bold << "\\____/"  << Format::Normal << Format::Green << " \\___\\__,_|_| |_|_| |_|\\___|_|   " << endl;
+		cout << endl;
+		cout << "           " << Format::Bold << "https" << Format::Normal << Format::Green << "://" << Format::Bold << "github.com" << Format::Normal << Format::Green << "/" << Format::Bold << "RoliSoft" << Format::Normal << Format::Green << "/" << Format::Bold << "Host-Scanner" << Format::Normal << Format::Default << endl;
+		cout << endl;
+		cout << "usage: " << app << " [args] targets" << endl;
+		cout << desc << endl;
+	}
+
+	if (vm.count("shodan-key") != 0)
+	{
+		cout << vm["shodan-key"].as<string>() << endl;
+	}
 
 #if HAVE_CURL
 	curl_global_cleanup();

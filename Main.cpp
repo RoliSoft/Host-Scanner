7434a534994bee7ba4439c09a5276c6b45e9383c
RoliSoft <rolisoft@gmail.com>
2015-12-20 04:48:01 +0200

    Added CLI handling.

--- Main.cpp
+++ Main.cpp
@@ -27,7 +27,9 @@
 #include "Stdafx.h"
 #include "Utils.h"
 #include "Format.h"
+#include "HostScanner.h"
 #include "InternalScanner.h"
+#include "NmapScanner.h"
 #include "ShodanScanner.h"
 #include "CensysScanner.h"
 
@@ -39,13 +41,182 @@ using namespace std;
 namespace fs = boost::filesystem;
 namespace po = boost::program_options;
 
+#define ERR 0
+#define MSG 1
+#define VRB 2
+#define DBG 3
+
+void log(int level, const string& msg)
+{
+	ostream* os;
+
+	switch (level)
+	{
+	case ERR:
+		os = &cerr;
+		cout << Format::Bold << Format::Red << "[!] " << Format::Default << Format::Normal;
+		break;
+	default:
+		os = &cout;
+		cout << Format::Green << "[*] " << Format::Default;
+		break;
+	}
+
+	*os << msg << endl;
+}
+
+void log(const string& msg)
+{
+	log(MSG, msg);
+}
+
+int scan(const po::variables_map& vm)
+{
+	int retval = EXIT_SUCCESS;
+
+	string p_scanner;
+	string p_port;
+	vector<string> p_target;
+
+	// get scanner
+
+	if (vm.count("scanner") != 0)
+	{
+		p_scanner = vm["scanner"].as<string>();
+	}
+	else
+	{
+		p_scanner = "internal";
+	}
+
+	HostScanner* scanner = nullptr;
+	if (p_scanner == "internal" || p_scanner.length() == 0)
+	{
+		scanner = new InternalScanner();
+	}
+	else if (p_scanner == "nmap")
+	{
+		scanner = new InternalScanner();// TODO NmapScanner();
+	}
+#if HAVE_CURL
+	else if (p_scanner == "shodan")
+	{
+		string key;
+
+		if (vm.count("shodan-key") != 0)
+		{
+			key = vm["shodan-key"].as<string>();
+		}
+
+		if (key.length() < 2)
+		{
+			log(ERR, "Shodan requires an API key via --shodan-key from https://account.shodan.io/");
+			retval = EXIT_FAILURE;
+			goto cleanup;
+		}
+
+		scanner = new ShodanScanner();
+		(reinterpret_cast<ShodanScanner*>(scanner))->key = key;
+	}
+	else if (p_scanner == "censys")
+	{
+		string key;
+
+		if (vm.count("censys-key") != 0)
+		{
+			key = vm["censys-key"].as<string>();
+		}
+
+		if (key.length() < 2 || key.find(":") == string::npos)
+		{
+			log(ERR, "Censys requires token in `uid:secret` format via --censys-key from https://censys.io/account");
+			retval = EXIT_FAILURE;
+			goto cleanup;
+		}
+
+		scanner = new CensysScanner();
+		(reinterpret_cast<CensysScanner*>(scanner))->auth = key;
+	}
+#else
+	else if (p_scanner == "shodan" || p_scanner == "censys")
+	{
+		log(ERR, "Scanner '" + p_scanner + "' is not available as this version of the binary was compiled without libcurl.");
+		retval = EXIT_FAILURE;
+		goto cleanup;
+	}
+#endif
+	else
+	{
+		log(ERR, "Scanner '" + p_scanner + "' is not supported.");
+		retval = EXIT_FAILURE;
+		goto cleanup;
+	}
+
+	// check passive
+
+	if (vm.count("passive") != 0 && !(p_scanner == "shodan" || p_scanner == "censys"))
+	{
+		log(ERR, "Scanner '" + p_scanner + "' is not passive.");
+		retval = EXIT_FAILURE;
+		goto cleanup;
+	}
+
+	// read ports
+
+	if (vm.count("port") != 0)
+	{
+		p_port = vm["port"].as<string>();
+	}
+
+	if (p_port == "-")
+	{
+		log(VRB, "Scanning all 65535 ports.");
+	}
+	else if (p_port.length() == 0)
+	{
+		log(VRB, "Scanning top 100 ports.");
+	}
+	else
+	{
+		// TODO read all listed ports
+	}
+
+	// read targets
+
+	if (vm.count("target") != 0)
+	{
+		p_target = vm["target"].as<vector<string>>();
+	}
+
+	if (p_target.size() == 0)
+	{
+		log(ERR, "No targets to scan.");
+		retval = EXIT_FAILURE;
+		goto cleanup;
+	}
+	else
+	{
+		// TODO read all targets
+	}
+
+	log("Initiating scan against ...");
+
+cleanup:
+	if (scanner != nullptr)
+	{
+		delete scanner;
+	}
+
+	return retval;
+}
+
 int main(int argc, char *argv[])
 {
 #if Windows
 	WSADATA wsaData;
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
-		cerr << "Failed to initialize WinSock." << endl;
+		log(ERR, "Failed to initialize WinSock.");
 		return EXIT_FAILURE;
 	}
 #endif
@@ -75,7 +246,7 @@ int main(int argc, char *argv[])
 			"  censys   - Uses data from Censys. (passive; requires API key)")
 		("shodan-key", po::value<string>(), "Specifies an API key for Shodan.")
 		("censys-key", po::value<string>(), "Sepcifies an API key for Censys in the `uid:secret` format.")
-		("passive,p", "Globally disables active reconnaissance. Functionality using active scanning will break, but ensures no accidental active scans will be initiated, which might get construed as hostile.")
+		("passive,x", "Globally disables active reconnaissance. Functionality using active scanning will break, but ensures no accidental active scans will be initiated, which might get construed as hostile.")
 		("no-logo,q", "Suppresses the ASCII logo.")
 		("version,v", "Display version information.")
 		("help,h", "Displays this message.")
@@ -87,7 +258,6 @@ int main(int argc, char *argv[])
 	po::variables_map vm;
 	po::store(po::command_line_parser(argc, argv).options(desc).positional(pos).run(), vm);
 
-	string path = "";
 	vector<string> paths = {
 #if Windows
 		get<0>(splitPath(getAppPath())) + "\\HostScanner.ini",
@@ -99,27 +269,23 @@ int main(int argc, char *argv[])
 #endif
 	};
 
-	for (auto check : paths)
+	for (auto path : paths)
 	{
-		fs::path fp(check);
+		fs::path fp(path);
 
 		if (fs::exists(fp) && fs::is_regular_file(fp))
 		{
-			path = check;
-			break;
+			po::store(po::parse_config_file<char>(path.c_str(), desc, true), vm);
 		}
-	}
-
-	if (path.length() != 0)
-	{
-		po::store(po::parse_config_file<char>(path.c_str(), desc, true), vm);
 	}
 
 	po::notify(vm);
 
+	int  retval  = EXIT_SUCCESS;
+	bool handled = false;
+
 	if (vm.count("no-logo") == 0)
 	{
-		auto app = get<1>(splitPath(getAppPath()));
 		cout << Format::Green;
 		cout << "  " << Format::Bold << " _   _ "  << Format::Normal << Format::Green << "          _    "   << Format::Bold << " _____"   << Format::Normal << Format::Green << "                                 "    << endl;
 		cout << "  " << Format::Bold << "| | | |"  << Format::Normal << Format::Green << "         | |   "   << Format::Bold << "/  ___|"  << Format::Normal << Format::Green << "                                "     << endl;
@@ -130,13 +296,39 @@ int main(int argc, char *argv[])
 		cout << endl;
 		cout << "           " << Format::Bold << "https" << Format::Normal << Format::Green << "://" << Format::Bold << "github.com" << Format::Normal << Format::Green << "/" << Format::Bold << "RoliSoft" << Format::Normal << Format::Green << "/" << Format::Bold << "Host-Scanner" << Format::Normal << Format::Default << endl;
 		cout << endl;
-		cout << "usage: " << app << " [args] targets" << endl;
-		cout << desc << endl;
 	}
 
-	if (vm.count("shodan-key") != 0)
+	if (vm.count("target") != 0 && !(vm.count("version") != 0 || vm.count("help") != 0))
 	{
-		cout << vm["shodan-key"].as<string>() << endl;
+		handled = true;
+		retval  = scan(vm);
+	}
+	else if (vm.count("version") != 0)
+	{
+		handled = true;
+
+		if (vm.count("no-logo") == 0)
+		{
+			cout << "           " << string((40 - (14 + strlen(VERSION_STRING))) / 2, ' ');
+		}
+
+		cout << Format::Green << Format::Bold << "Host Scanner v" << VERSION_STRING << Format::Normal << Format::Default << endl;
+
+		if (vm.count("no-logo") == 0)
+		{
+			cout << endl;
+		}
+	}
+	else // if (vm.count("help") != 0)
+	{
+		auto app = get<1>(splitPath(getAppPath()));
+		cout << "usage: " << app << " [args] targets" << endl;
+		cout << desc << endl;
+
+		if (vm.count("help") != 0)
+		{
+			handled = true;
+		}
 	}
 
 #if HAVE_CURL
@@ -145,8 +337,7 @@ int main(int argc, char *argv[])
 
 #if Windows
 	WSACleanup();
-	system("pause");
 #endif
 
-	return EXIT_SUCCESS;
+	return !handled ? EXIT_FAILURE : retval;
 }

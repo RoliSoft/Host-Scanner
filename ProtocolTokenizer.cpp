466d8a5e5a499641c976e3d63bd0db9b92ad5454
RoliSoft <rolisoft@gmail.com>
2016-03-01 18:39:42 +0200

    Implemented ProtocolTokenizer::AutoTokenize.

--- ProtocolTokenizer.cpp
+++ ProtocolTokenizer.cpp
@@ -1,11 +1,32 @@
 #include "ProtocolTokenizer.h"
+#include "HttpTokenizer.h"
+#include "ThreeDigitTokenizer.h"
 
 using namespace std;
 
 vector<string> ProtocolTokenizer::AutoTokenize(const string& banner)
 {
-	// TODO
-	return vector<string>();
+	// primitive implementation for now, later perhaps register implementations
+	// into an ordered_map by protocol popularity and call CanTokenize() on each
+
+	static HttpTokenizer http;
+
+	if (http.CanTokenize(banner))
+	{
+		return http.Tokenize(banner);
+	}
+
+	static ThreeDigitTokenizer tdt;
+
+	if (tdt.CanTokenize(banner))
+	{
+		return tdt.Tokenize(banner);
+	}
+
+	// if no protocol-specific tokenizer is available,
+	// return the whole string as a token
+
+	return vector<string> { banner };
 }
 
 ProtocolTokenizer::~ProtocolTokenizer()

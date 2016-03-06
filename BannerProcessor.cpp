82fbb0fc44ac53a42b76def4e18f03e2997af265
RoliSoft <rolisoft@gmail.com>
2016-03-06 19:24:55 +0200

    Added AutoProcess() to BannerProcessor.

--- BannerProcessor.cpp
+++ BannerProcessor.cpp
@@ -1,4 +1,7 @@
 #include "BannerProcessor.h"
+#include "ServiceRegexMatcher.h"
+#include "CpeDictionaryMatcher.h"
+#include "ProtocolTokenizer.h"
 
 using namespace std;
 
@@ -10,6 +13,55 @@ void BannerProcessor::Scan(Service* service)
 	{
 		service->cpe.insert(service->cpe.end(), matches.begin(), matches.end());
 	}
+}
+
+vector<string> BannerProcessor::AutoProcess(const string& banner)
+{
+	vector<string> cpes;
+
+	// the regular expression pattern matcher implementation generally matches
+	// against full service banners and as a result it is much more accurate
+
+	static ServiceRegexMatcher srm;
+
+	auto rmlst = srm.Scan(banner);
+
+	if (rmlst.size() > 0)
+	{
+		cpes.insert(cpes.end(), rmlst.begin(), rmlst.end());
+	}
+
+	// the CPE dictionary entry matcher requires tokenization in order to be
+	// more precise
+
+	static CpeDictionaryMatcher cdm;
+	
+	auto tokens = ProtocolTokenizer::AutoTokenize(banner);
+
+	for (auto token : tokens)
+	{
+		auto cdlst = cdm.Scan(token);
+
+		if (cdlst.size() > 0)
+		{
+			cpes.insert(cpes.end(), cdlst.begin(), cdlst.end());
+		}
+	}
+
+	if (cpes.size() == 0)
+	{
+		// if neither methods matched, re-try matching the full service banner
+		// with the CPE dictionary matcher as a last resort
+		
+		auto cdlst = cdm.Scan(banner);
+
+		if (cdlst.size() > 0)
+		{
+			cpes.insert(cpes.end(), cdlst.begin(), cdlst.end());
+		}
+	}
+
+	return cpes;
 }
 
 BannerProcessor::~BannerProcessor()
--- BannerProcessor.h
+++ BannerProcessor.h
@@ -26,6 +26,15 @@ public:
 	virtual std::vector<std::string> Scan(const std::string& banner) = 0;
 
 	/*!
+	 * Tries to processes the specified service banner with all known implementations of this class.
+	 *
+	 * \param banner Service banner.
+	 * 
+	 * \return Matching CPE entries.
+	 */
+	static std::vector<std::string> AutoProcess(const std::string& banner);
+
+	/*!
 	 * Frees up the resources allocated during the lifetime of this instance.
 	 */
 	virtual ~BannerProcessor();
